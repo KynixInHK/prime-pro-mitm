5d203dc94d8877ff9aefcf89f91f3bb5911d4e90
KynixInHK <kynixcn@gmail.com>
2024-05-30 17:09:55 +0800

    ykb

--- ykb.js
+++ ykb.js
@@ -8,18 +8,24 @@
 
  [rewrite_local]
  ^https?:\/\/api\.yikaobang\.com\.cn\/index\.php\/user\/main\/vip.*$ url script-response-body https://raw.githubusercontent.com/KynixInHK/prime-pro-mitm/master/ykb.js
-
+ ^https?:\/\/api\.yikaobang\.com\.cn\/index\.php\/User\/Main\/getUserInfo url script-response-body https://raw.githubusercontent.com/KynixInHK/prime-pro-mitm/master/ykb.js
  [mitm]
  hostname = api.yikaobang.com.cn
 
  *******************************/
 
+var url = $request.url
 var body = $response.body
 var obj = JSON.parse(body)
-obj.data.is_vip = "1"
-obj.data.vip_deadline = "2099-12-31 到期"
-obj.data.vip.is_vip = 1
-obj.data.vip.simple_declaration = "2099-12-31 到期"
-obj.data.vip.vip_deadline = "2099-12-31 到期"
+if (url.includes("/vip")) {
+    obj.data.is_vip = "1"
+    obj.data.vip_deadline = "2099-12-31 到期"
+    obj.data.vip.is_vip = 1
+    obj.data.vip.simple_declaration = "2099-12-31 到期"
+    obj.data.vip.vip_deadline = "2099-12-31 到期"
+} else if (url.includes("/getUserInfo")){
+    obj.data.is_vip = "1"
+    obj.data.vip_deadline = "2099-12-31 到期"
+}
 body = JSON.stringify(obj)
 $done(body)
